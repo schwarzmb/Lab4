e6a49f134b1496d2db7f5c76ea7cff29897397b1
Matthew Schwarz <schwarz1016@gmail.com>
2018-03-08 18:00:33 -0500

    webview

--- app/src/main/java/com/example/matthewschwarz/lab4/MainActivity.java
+++ app/src/main/java/com/example/matthewschwarz/lab4/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.matthewschwarz.lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("https://www.google.com");
+
     }
 }
